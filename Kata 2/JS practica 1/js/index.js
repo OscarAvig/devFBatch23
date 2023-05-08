63df718cd88c2c8d75a67b143f4b17b6c3342eaf
OscarAvig <avig517@gmail.com>
2023-05-08 15:14:56 -0600

    up

---
+++ Kata 2/JS practica 1/js/index.js
@@ -0,0 +1,30 @@
+//console.log('HOLA DESDE MI ARCHIVO JS') 
+
+// alert ('Bienvenidos a mi primer sitio con js')
+
+// document.write('Vengo del archivo js')
+
+// document.write('Bienvenido ', prompt('Ingresa tu nombre'))
+
+/*alert ('Bienvenidos a mi primer sitio con js')*/
+
+
+// var nombre ='Miguel'
+// nombre ='Leticia'
+// var nombre ='Cristian'
+
+// console.log(nombre)
+
+const color ='amarillo'
+if( color == 'Verde'){
+    console.log ('Puebes pasar')
+}
+else if (color == 'amarillo'){
+    console.log ('Puedes pasar solo que con precaución')
+}
+else if ( color =='Rojo') {
+    console.log ('Espera a que el semáforo esté en verde')
+}
+else{
+    console.log('ingresa un color valido')
+} 
